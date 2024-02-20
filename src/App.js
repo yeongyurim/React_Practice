26db0a62c07469f5b0ddfa0ef78684f8fcaba052
zxc991019 <zxc991019@naver.com>
2024-02-20 19:43:11 +0900

    library

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    )
-  }
-}
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc ="HTML is HyperText Markup Language."></Content>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode:'read',
+            selected_content_id: 2,
+            subject:{title:'WEB', sub:'World Wide Web!'},
+            welcome:{title:'Welcome', desc:'Hello,React!!'},
+            contents:[
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'CSS is for design'},
+                {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+            ]
+        }
+    }  
+    render() {
+        console.log('App render');
+        var _title, _desc = null;
+        if( this.state.mode === 'welcome' ) {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if ( this.state.mode === 'read' ) {
+            var i = 0;
+            while(i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if(data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+                    break;
+                }
+                i = i + 1;
+            }
+        }
+        console.log("render", this); // render 함수 내에서의 this는 현재 컴포넌트를 가르킨다.
+        return (
+            <div className="App">
+            
+            <Subject 
+                title = { this.state.subject.title } // 중괄호로 감싼 코드는 자바 스크립트 코드로 인식됌.
+                sub = { this.state.subject.sub } // double quotation은 문자열로 취급됨.
+                onChangePage = { function () {
+                    this.setState({mode:'welcome'});
+                }.bind(this)}
+            ></Subject>
+            <TOC 
+                onChangePage = { function() {
+                    this.setState({mode:'read'});
+                }.bind(this)}
+                data={ this.state.contents }></TOC>
+            <Content title = {_title } desc = { _desc }></Content>
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
     render() {
+        console.log('TOC render');
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        // element를 자동적으로 생성하는 경우 각 리스트 항목에 키 값을 설정해주어야 리액트 내부적으로 문제가 생기지 않는다.
+        while(i < data.length) {
+            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}
+                onClick={function (e) {
+                    e.preventDefault();
+                    this.props.onChangePage();
+                }.bind(this)}
+                >{data[i].title}</a></li>);
+            i = i + 1;
+        }
         return (
             <nav>
                 <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
